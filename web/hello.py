614a309a6bdcb5edebdc9b5bc1c7163a9cb758a8
hdnseeker <vlasovroman2017@gmail.com>
2020-09-10 12:56:05 +0300

    gunicorn (wsgi) + nginx

--- web/hello.py
+++ web/hello.py
@@ -1,17 +1,8 @@
-from cgi import parse_qsl
-
 def application(environ, start_response):
-	d = parse_qsl(environ['QUERY_STRING'])
-	output = '';
-	if environ['REQUEST_METHOD'] == 'GET':
-		for ch in d:
-			output += ('='.join(ch))
-			output += ('\n')
-	output_len = sum(len(line) for line in output)	
 	status = '200 Ok'
-	response_headers = [('Content-type', 'text/plain'),
-						('Content-Length', str(output_len))]
+	response_headers = [('Content-type', 'text/plain')]
+						
 
 	start_response(status, response_headers)
-	return [output.encode('utf-8')]
+	return [(i + '\n').encode('utf-8') for i in environ['QUERY_STRING'].split('&')]
 	

bc3eb3cbd7cafe3595391f850e62708734303247
hdnseeker <vlasovroman2017@gmail.com>
2020-09-10 23:10:43 +0300

    gunicorn (wsgi) + nginx

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+	title = models.TextField()
+	text = models.CharField()
+	added_at = models.DateTimeField(auth_now_add=True) 
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User)
+	objects = QuestionManager()
+
+
+
+class Answer(models.Model):
+	text = models.CharField()
+	added_at = models.DateTimeField(auth_now_add=True) 
+	question = models.ForeignKey(Question)
+	author = models.ForeignKey(User)
+
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_ad')
+	def popular(self)
+		return self.order_by('-rating')

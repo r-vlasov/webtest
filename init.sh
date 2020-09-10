d911e57753cfca0e208c04792d63d960d29901c7
hdnseeker <vlasovroman2017@gmail.com>
2020-09-10 12:46:44 +0300

    gunicorn (wsgi) + nginx

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 # initialize environment
 
 sudo cp ./nginx.conf /etc/nginx/nginx.conf
-gunicorn -c ./etc/gunicorn.py hello:application &> ./gunicorn.access.log &
+cd ./web
+gunicorn -c ./../etc/gunicorn.py hello:application &> ./gunicorn.access.log &
 sudo nginx

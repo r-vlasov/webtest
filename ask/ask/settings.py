cd476448535ca0a76da90eb5776cb7c56c7d595c
hdnseeker <vlasovroman2017@gmail.com>
2020-09-10 23:37:05 +0300

    asd

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6(im6(4$el!$-wr9=8i9_9qldn#qnze7l6+6ck&8op1))cs2dy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'stepic_web',
+		'USER': 'box',
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,23 +3,22 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
 	title = models.TextField()
-	text = models.CharField()
-	added_at = models.DateTimeField(auth_now_add=True) 
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now=True) 
 	rating = models.IntegerField(default=0)
-	author = models.ForeignKey(User)
-	likes = models.ManyToManyField(User)
-	objects = QuestionManager()
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
 
 class Answer(models.Model):
-	text = models.CharField()
-	added_at = models.DateTimeField(auth_now_add=True) 
-	question = models.ForeignKey(Question)
-	author = models.ForeignKey(User)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now=True) 
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class QuestionManager(models.Manager):
 	def new(self):
 		return self.order_by('-added_ad')
-	def popular(self)
+	def popular(self):
 		return self.order_by('-rating')
